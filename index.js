fdb6f77b8edc48e212faf62ab822b852212aa270
msibrahimhussain <141616747+msibrahimhussain@users.noreply.github.com>
2023-12-08 19:34:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,17 +16,13 @@ class CoinToss extends Component {
     const n = parseInt(Math.random() * 10)
     if (n < 5) {
       this.setState({toss: 0})
-    } else {
-      this.setState({toss: 1})
-    }
-    const {toss} = this.state
-    if (toss === 0) {
       this.setState(prevState => ({Total: prevState.Total + 1}))
       this.setState(prevState => ({Tails: prevState.Tails + 1}))
       this.setState({
         imageUrl: 'https://assets.ccbp.in/frontend/react-js/tails-img.png',
       })
     } else {
+      this.setState({toss: 1})
       this.setState(prevState => ({Total: prevState.Total + 1}))
       this.setState(prevState => ({Heads: prevState.Heads + 1}))
       this.setState({
